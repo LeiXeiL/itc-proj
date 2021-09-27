7621932cf9c118f31eb2c67f1c35d78021805284
LeiXeiL <Bonnie517yt@gmail.com>
2021-09-27 19:26:27 +0400

    Controls is completed
    FUUUUUU....

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,9 +3,14 @@
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject Camera;
-    [SerializeField] private float speed, sens;
+    [SerializeField] private float sens;
 
-    private float hor, vert, MouseX, MouseY, angleX;
+    private float hor, vert, MouseX, MouseY, angleX, speed;
+
+    private void Start()
+    {
+        speed = 6;
+    }
 
     void Update()
     {
@@ -16,13 +21,31 @@ public class PlayerController : MonoBehaviour
         angleX = Mathf.Clamp(angleX, -60, 60);
         Camera.transform.localRotation = Quaternion.Euler(angleX, 0, 0);
         transform.Rotate(new Vector3(0, MouseX, 0));
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 12;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 6;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            transform.localScale = new Vector3(.5f, .5f, .5f);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 
     private void FixedUpdate()
     {
         hor = Input.GetAxis("Horizontal");
         vert = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * speed * vert);
-        transform.Translate(Vector3.right * speed * hor);
+        transform.Translate(Vector3.forward * speed * vert * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * hor * Time.deltaTime);
     }
 }
---
+++ Assets/scripts/PlayerInteraction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] private GameObject ToHandPos;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 3))
+            {
+                if (hit.collider.CompareTag("Interactable") && transform.childCount == 1)
+                {
+                    GameObject item = hit.collider.gameObject;
+                    item.transform.SetParent(gameObject.transform);
+                    //item.transform.position = new Vector3(0.468f, -0.303f, 0.816f);
+                    //item.transform.rotation = Quaternion.Euler(1, 1, 1);
+                    item.transform.position = ToHandPos.transform.position;
+                    item.transform.rotation = ToHandPos.transform.rotation;
+
+                    if (item.GetComponent<Rigidbody>() != null)
+                    {
+                        item.GetComponent<Rigidbody>().isKinematic = true;
+                    }
+                    if (item.GetComponent<Collider>() != null)
+                    {
+                        item.GetComponent<Collider>().isTrigger= true;
+                    }
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && transform.childCount >= 2)
+        {
+            var item = transform.GetChild(1);
+            item.SetParent(null);
+            if (item.GetComponent<Rigidbody>() != null)
+            {
+                item.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            if (item.GetComponent<Collider>() != null)
+            {
+                item.GetComponent<Collider>().isTrigger = false;
+            }
+        }
+    }
+}
